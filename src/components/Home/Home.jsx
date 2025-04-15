55aa5eb45f8da5aaf86ff2937a05d0a8bf384746
Thiago <thiiagoo106@gmail.com>
2025-04-15 16:19:47 -0300

    Animação Saiba Mais

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,42 +1,49 @@
 import styles from './Home.module.css'
 import banner from '../../img/banner.png'
 import ApresentationCard from './ApresentationCard'
-
-
+import { useState } from 'react'
 
 function Home() {
+  const [mostrarPagina, setMostrarPagina] = useState(false)
 
-      function viewPage(){
-            alert("Funcionando")
-      }
+  const ativarPagina = () => {
+    setMostrarPagina(true)
+  }
 
-      return (
-            <>
-                  <section className={styles.HomeContainer}>
+  const fecharPagina = () => {
+    setMostrarPagina(false)
+  }
 
-                        <video
-                              autoPlay
-                              muted
-                              loop
-                              className={styles.videoBackground}
-                        >
-                              <source src="https://videos.pexels.com/video-files/3176015/3176015-hd_1920_1080_25fps.mp4" type="video/mp4" />
-                        </video>
+  return (
+    <>
+      <section className={styles.HomeContainer}>
+        <video
+          autoPlay
+          muted
+          loop
+          className={styles.videoBackground}
+        >
+          <source
+            src="https://videos.pexels.com/video-files/3176015/3176015-hd_1920_1080_25fps.mp4"
+            type="video/mp4"
+          />
+        </video>
 
-                        <div className={styles.apresentationContainer}>
-                              <h1> Olá, eu sou
-                                    <span className={styles.nome}>Thiago Marques</span>
-                              </h1>
-                              <p className={styles.textDeveloper}>Desenvolvedor Front-end</p>
-                              <button className={styles.aboutMe} onClick={viewPage}>Saba Mais</button>
-                        </div>
-                        <ApresentationCard/>
-                  </section>
+        <div className={styles.apresentationContainer}>
+          <h1>
+            Olá, eu sou
+            <span className={styles.nome}> Thiago Marques</span>
+          </h1>
+          <p className={styles.textDeveloper}>Desenvolvedor Front-end</p>
+          <button className={styles.aboutMe} onClick={ativarPagina}>
+            Saiba Mais
+          </button>
+        </div>
 
-
-            </>
-      )
-
+        <ApresentationCard isVisible={mostrarPagina} onClose={fecharPagina} />
+      </section>
+    </>
+  )
 }
 
 export default Home
