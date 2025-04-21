4e06a429fe3aaf5ed987b16077d0e7714d0a841a
Thiago Marques <thiiagoo106@gmail.com>
2025-04-21 16:09:21 -0300

    Responsividade Links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,35 @@
 import styles from './Nav.module.css'
+import home from '../../img/home.png'
+import webDevelopment from '../../img/web-development.png'
+import project from '../../img/project.png'
+import contact from '../../img/contact.png'
 
 function Nav(){
       return(
             <>
                   <nav>
-                        <a href="#" className={styles.navLink}>HOME</a>
-                        <a href="#" className={styles.navLink}>TECNOLOGIAS</a>
-                        <a href="#" className={styles.navLink}>PROJETOS</a>
-                        <a href="#" className={styles.navLink}>CONTATO</a>
+                        <div className={styles.navItens}>
+                              <a href="#" className={styles.navLink}>
+                                    <img src={home} alt="" />
+                                    HOME
+                              </a>
+
+                              <a href="#" className={styles.navLink}>
+                                    <img src={webDevelopment} alt="" />
+                                    TECNOLOGIAS
+                              </a>
+
+                              <a href="#" className={styles.navLink}>
+                                    <img src={project} alt="" />
+                                    PROJETOS
+                              </a>
+
+                              <a href="#" className={styles.navLink}>
+                                    <img src={contact} alt="" />
+                                    CONTATO
+                              </a>
+                        </div>
+
                   </nav>
             </>
       )
